d6676c7911ca9191fe88835255cfe5c252c47d79
orangejoa <orangesore@gmail.com>
2024-06-25 09:18:29 +0900

    testing Google OAuth 2.0

--- src/main/java/com/auth/mfa/config/SecurityConfig.java
+++ src/main/java/com/auth/mfa/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorizeRequests ->
+                .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/","/login**").permitAll()
                                 .anyRequest().authenticated()
